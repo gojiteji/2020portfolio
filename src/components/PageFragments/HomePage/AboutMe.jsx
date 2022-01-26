11de765dbe3e454fb864290a50414aed5d44f7f7
Koki Tanaka <mail@gojiteji.com>
2022-01-26 15:48:19 +0900

    fixed contact info

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         
         <h1 className="titleSeparate">About Me</h1>
         <p>
-        Hello. Thank you for visiting this site. I am currently a fourth year universtiy student in Japan. My technical fields of interest are NLP and backend engineer. I mainly code in Python and Go. If you have any questions, please feel free to contact me via the left contact button.
+        Hello. Thank you for visiting this site. I am currently a fourth year universtiy student in Japan. My technical fields of interest are NLP and backend engineer. I mainly code in Python and Go. If you have any questions, please feel free to contact me via my e-mail.
         </p>
         
       </div>
