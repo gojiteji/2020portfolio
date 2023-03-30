7d491b3feff5039634b6f794b2423364b30c553b
Koki Tanaka <mail@gojiteji.com>
2023-03-30 18:42:47 +0900

    add mstdn

--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -12,7 +12,7 @@ import Config from '../../../../config';
 
 const { Content } = Layout;
 const {
-  facebook, github, linkedin,huggingface, twitter,sidmoid.social,
+  facebook, github, linkedin,huggingface, twitter,sidmoid,
 } = Config.social;
 
 const content = (
