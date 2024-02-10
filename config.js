f492890566f32a716e782665f405993d319a2d73
gojiteji <develop.notel@gmail.com>
2024-02-11 00:22:07 +0900

    mstdn -> facebook

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ pages: {
     linkedin: 'https://www.linkedin.com/in/koki-tanaka-6bb38a16a',
     huggingface: 'https://huggingface.co/gojiteji',
     sigmoid: 'https://sigmoid.social/@gojiteji',
+    facebook: 'https://www.facebook.com/gojiteji/',
   },
   contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
   googleAnalyticTrackingId: process.env.GA_TRACKING_ID || 'UA-143776882-1',
