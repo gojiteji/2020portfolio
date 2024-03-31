345e0783f6ef1e1d304f4a3175994b281bfff5c7
gojiteji <develop.notel@gmail.com>
2024-04-01 00:02:22 +0900

    profile update

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         
         <h1 className="titleSeparate">About Me</h1>
         <p>
-        Hello. Thank you for visiting this site. I am currently a second-year master's student in Japan. My technical fields of interest are NLP and web development. I mainly code in Python and Go. I also like to use HuggingFace and JAX. If you have any questions, please feel free to contact me via my e-mail.
+        Hello. Thank you for visiting this site. I am currently CEO at mocomoco inc. & a phd student in NAIST NLP Lab, Japan. My technical fields of interest are NLP and web development. I mainly code in Python and Go. I also like to use HuggingFace and JAX. If you have any questions, please feel free to contact me via my e-mail.
         </p>
         
       </div>
